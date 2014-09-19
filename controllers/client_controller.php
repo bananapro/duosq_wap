bbd950fceebd83f87d25c21453f24d7adabcaeb8
jon.huang <sakura.speed@gmail.com>
2014-09-20 00:40:25 +0800

    调整client页面link模板

--- controllers/client_controller.php
+++ controllers/client_controller.php
@@ -4,10 +4,18 @@ class ClientController extends AppController {
 
 	var $name = 'Client';
 
+	//APP下载引导页
 	function index(){}
 
+	//拷贝下载链接
 	function link(){
 
+		if(getBrowser()=='ios'){
+			$link = C('comm', 'app_download_ios');
+		}else{
+			$link = C('comm', 'app_download_android');
+		}
+		$this->set('link', $link);
 		$this->set('hide_nav', true);
 		$this->set('hide_footer', true);
 	}
