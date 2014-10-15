4057005b4b3239d3179bb767e436336a0f98b729
jon.huang <sakura.speed@gmail.com>
2014-10-15 11:23:47 +0800

    APP WAP加入跟单，APP(IOS)由于loading BUG未加入跟单

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -24,4 +24,27 @@ function promoMidcatUrl($midcat){
 	if(!$midcat)return '/';
 	return MY_HOMEPAGE_URL.'/promo-midcat-'.urlencode($midcat);
 }
+
+//转换外链
+function promoUrl($sp, $goods_id, $url, $tc='wap'){
+
+	if(!$sp || !$goods_id){
+		return "javascript:alert('链接错误，请稍后尝试！');\" target='_self'";
+	}
+
+	//if(isset($_GET['tc']))$tc = $_GET['tc'];
+
+	//获取跳转驱动
+	if(D('go')->getDriver($sp)){
+
+		if($sp == 'tmall' || $sp == 'taobao'){
+			//淘宝移动端hack跳转跟单
+			$url = MY_WWW_URL .'/item-'.$sp.'-'.$goods_id.'?tc='.$tc;
+		}else{
+			$url = MY_WWW_URL . "/go/{$sp}?tc={$tc}&t=".urlencode($url);
+		}
+	}
+
+	return $url;
+}
 ?>
